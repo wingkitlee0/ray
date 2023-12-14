c09dfd6907bc78c801f51e4c50d68713fbeb30a4
Kit Lee <wklee4993@gmail.com>
2023-12-13 23:00:38 -0500

    first working version of repartition_by_column

--- python/ray/data/_internal/planner/exchange/split_task_spec.py
+++ python/ray/data/_internal/planner/exchange/split_task_spec.py
@@ -1,16 +1,8 @@
-from typing import List, Tuple, TypeVar, Union
-
-import numpy as np
-
-from ray.data._internal.delegating_block_builder import DelegatingBlockBuilder
-from ray.data._internal.planner.exchange.interfaces import ExchangeTaskSpec
-from ray.data._internal.progress_bar import ProgressBar
-from ray.data._internal.remote_fn import cached_remote_fn
-from ray.data._internal.sort import SortKey
-from ray.data.block import Block, BlockAccessor, BlockExecStats, BlockMetadata
-from ray.types import ObjectRef
+from typing import Any, List, Tuple, TypeVar, Union
 
 from ray.data._internal.boundaries import get_group_boundaries
+from ray.data._internal.planner.exchange.interfaces import ExchangeTaskSpec
+from ray.data.block import Block, BlockAccessor, BlockExecStats, BlockMetadata
 
 T = TypeVar("T")
 
@@ -51,20 +43,23 @@ class SplitTaskSpec(ExchangeTaskSpec):
 
         if len(split_indices) == 2:
             out = [block]
+            out_keys = [get_key_from_block(block, keys)]
         else:
-            out: List[Block] = [
-                accessor.slice(start, end)
-                for start, end in zip(split_indices[:-1], split_indices[1:])
-            ]
+            out: List[Block] = []
+            out_keys = []
+            for start, end in zip(split_indices[:-1], split_indices[1:]):
+                _block = accessor.slice(start, end)
+                _keys = get_key_from_block(_block, keys)
+                out.append(_block)
+                out_keys.append(_keys)
 
-        meta = accessor.get_metadata(
-            input_files=None, exec_stats=stats.build()
-        )
+        meta = accessor.get_metadata(input_files=None, exec_stats=stats.build())
         yield from out
         yield meta
+        yield out_keys
 
     @staticmethod
-    def reduce(
+    def reduce_idenity(
         keys: Union[str, List[str]],
         *mapper_outputs: List[Block],
         partial_reduce: bool = False,
@@ -73,10 +68,33 @@ class SplitTaskSpec(ExchangeTaskSpec):
         stats = BlockExecStats.builder()
         accessor = BlockAccessor.for_block(mapper_outputs[0])
 
-        meta = accessor.get_metadata(
-            input_files=None, exec_stats=stats.build()
-        )
+        meta = accessor.get_metadata(input_files=None, exec_stats=stats.build())
         yield from mapper_outputs
         yield meta
 
+    @staticmethod
+    def reduce(
+        keys: Union[str, List[str]],
+        *mapper_outputs: List[Block],
+        partial_reduce: bool = False,
+    ) -> Tuple[Block, BlockMetadata]:
+        import pyarrow as pa
 
+        stats = BlockExecStats.builder()
+        blocks = [BlockAccessor.for_block(block).to_arrow() for block in mapper_outputs]
+        out_block = pa.concat_tables(blocks)
+        meta = BlockAccessor.for_block(out_block).get_metadata(
+            input_files=None,
+            exec_stats=stats.build(),
+        )
+        return out_block, meta
+
+
+def get_key_from_block(block: Block, keys: Union[str, List[str]]) -> Tuple[Any]:
+    accessor = BlockAccessor.for_block(block)
+    arr = accessor.to_numpy(keys)
+
+    if isinstance(arr, dict):
+        return tuple([v[0] for v in arr.values()])
+    else:
+        return (arr[0],)
